1be13c9534e352c8cf0e8ae7c0638f0597914919
sunnice <13027619526@163.com>
2019-04-26 17:17:51 +0800

    。。。

--- portal/src/main/java/com/sunnice/portal/Controller/PmsSkuController.java
+++ portal/src/main/java/com/sunnice/portal/Controller/PmsSkuController.java
@@ -1,40 +1,38 @@
-package com.sunnice.portal.Controller;
-
-import com.sunnice.generator.assist.CommonResult;
-import com.sunnice.generator.entity.PmsSkuStock;
-import com.sunnice.portal.service.PmsSkuService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
-
-/**
- * @Author: JYD
- * @Date: 2019/4/26 12:12
- */
-@Api(tags = "PmsSkuController",description = "商品属性管理")
-@Controller
-@RequestMapping("/sku")
-public class PmsSkuController {
-    @Autowired
-    private PmsSkuService skuService;
-
-    @ApiOperation("列表查询")
-    @RequestMapping(value = "/queryList",method = RequestMethod.POST)
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "pageNum" ,value = "页码（默认第一页）",defaultValue = "1",dataType = "String",paramType = "query",required = false),
-            @ApiImplicitParam(name = "pageSize",value = "数量（默认每页10条）",defaultValue = "10",dataType = "String",paramType = "query",required = false)
-    })
-    @ResponseBody
-    public Object queryList(Integer pageNum,Integer pageSize){
-        List<PmsSkuStock> pmsSkuStocks = skuService.queryList(pageNum, pageSize);
-        return new CommonResult().pageSuccess(pmsSkuStocks);
-    }
-}
+//package com.sunnice.portal.Controller;
+//
+//import com.sunnice.generator.assist.CommonResult;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiImplicitParam;
+//import io.swagger.annotations.ApiImplicitParams;
+//import io.swagger.annotations.ApiOperation;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import java.util.List;
+//
+///**
+// * @Author: JYD
+// * @Date: 2019/4/26 12:12
+// */
+//@Api(tags = "PmsSkuController",description = "商品属性管理")
+//@Controller
+//@RequestMapping("/sku")
+//public class PmsSkuController {
+//    @Autowired
+//    private PmsSkuService skuService;
+//
+//    @ApiOperation("列表查询")
+//    @RequestMapping(value = "/queryList",method = RequestMethod.POST)
+//    @ApiImplicitParams({
+//            @ApiImplicitParam(name = "pageNum" ,value = "页码（默认第一页）",defaultValue = "1",dataType = "String",paramType = "query",required = false),
+//            @ApiImplicitParam(name = "pageSize",value = "数量（默认每页10条）",defaultValue = "10",dataType = "String",paramType = "query",required = false)
+//    })
+//    @ResponseBody
+//    public Object queryList(Integer pageNum,Integer pageSize){
+//        List<PmsSkuStock> pmsSkuStocks = skuService.queryList(pageNum, pageSize);
+//        return new CommonResult().pageSuccess(pmsSkuStocks);
+//    }
+//}
--- portal/src/test/java/com/sunnice/portal/PortalApplicationTests.java
+++ portal/src/test/java/com/sunnice/portal/PortalApplicationTests.java
@@ -1,7 +1,5 @@
 package com.sunnice.portal;
 
-import com.sunnice.generator.entity.PmsSkuStock;
-import com.sunnice.portal.service.PmsSkuService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,21 +11,21 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PortalApplicationTests {
-	@Autowired
-	private PmsSkuService skuService;
-
-	@Test
-	public void contextLoads() {
-		List<PmsSkuStock> pmsSkuStocks = skuService.queryList(1, 10);
-		for (PmsSkuStock skuStock:pmsSkuStocks){
-			System.out.println(skuStock.toString());
-		}
-	}
-
-	@Test
-	public void contextLoads01() {
-		Integer count = skuService.queryCount(36l);
-		System.out.println("Integer"+count);
-	}
+//	@Autowired
+//	private PmsSkuService skuService;
+//
+//	@Test
+//	public void contextLoads() {
+//		List<PmsSkuStock> pmsSkuStocks = skuService.queryList(1, 10);
+//		for (PmsSkuStock skuStock:pmsSkuStocks){
+//			System.out.println(skuStock.toString());
+//		}
+//	}
+//
+//	@Test
+//	public void contextLoads01() {
+//		Integer count = skuService.queryCount(36l);
+//		System.out.println("Integer"+count);
+//	}
 
 }
